7112469f45f4833df4f234c2ac0c388f5781abcb
Saheb Giri <iamsahebgiri@gmail.com>
2022-02-28 12:01:06 +0530

    fix: :bug: wish this work

--- app/routes/memes/random.tsx
+++ app/routes/memes/random.tsx
@@ -1,6 +1,5 @@
 import { memes } from '~/data/memes';
 
 export const loader = async () => {
-  const meme = memes.at(Math.floor(Math.random() * memes.length));
-  return meme;
+  return memes[Math.floor(Math.random() * memes.length)];
 };
